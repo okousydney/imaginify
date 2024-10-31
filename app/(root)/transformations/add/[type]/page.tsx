97b0ae8de9f390c46a40c5525ed57e1f3c96a446
24s-s.okou <s.okou@24s.com>
2024-10-31 21:27:47 +0100

    react hook form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,33 @@
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { title, subTitle, type: typeKey } = transformationTypes[type];
+  const { userId } = await auth();
+
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header title={title} subtitle={subTitle} />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={typeKey as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
