949763c38adbf8596ededb939d8c1afb9961bfd8
24s-s.okou <s.okou@24s.com>
2024-11-03 20:10:19 +0100

    middleware public routes 2

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
   "/",
   "/api/webhooks/clerk",
   "/api/webhooks/stripe",
